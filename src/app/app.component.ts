ddfb9247a5dc7f41cce2533fad0d6ac534097536
Abdallah <bratosab@gmail.com>
2022-06-30 10:58:57 +0200

    feat(signup): Added input for username

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { SetUsername } from './core/app.actions';
+import { AppState } from './core/app.reducers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-state-management-ng14';
+  state$: Observable<AppState>;
+  username: string = '';
+
+  constructor(private store: Store<{ app: AppState }>) {
+    this.state$ = this.store.select((state) => state.app);
+  }
+
+  setUsername(username: string) {
+    this.store.dispatch(SetUsername({ username }));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import {
+  routerReducer,
+  StoreRouterConnectingModule,
+} from '@ngrx/router-store';
 import { appReducer } from './core/app.reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ app: appReducer }, {}),
+    FormsModule,
+    StoreModule.forRoot(
+      { app: appReducer, router: routerReducer },
+      {}
+    ),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
